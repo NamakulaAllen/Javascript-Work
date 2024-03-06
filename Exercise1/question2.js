ac8da5ed7c21841ffb901edcf219057a10444b64
NamakulaAllen <143063952+NamakulaAllen@users.noreply.github.com>
2024-03-06 16:24:53 +0300

    Add files via upload
    Javascript

---
+++ Exercise1/question2.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: "Jane",
+    lastName: "Doe"
+  };
+  
+  // Adding the country property
+  person.country = "Uganda";
+  console.log(person)
+  
---
+++ functions.js
@@ -0,0 +1,19 @@
+// Calculate the sum of two numbers, and return the result
+
+const num1 = 10;
+const num2 = 20;
+
+const sum = num1 + num2;
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+// Javascript function to square the numbers
+function squareNumbers(number) {
+    return number * number; 
+  }
+  
+  for (let i = 1; i <= 10; i++) {
+    const squaredValue = squareNumbers(i);
+    console.log(`The square of ${i} is ${squaredValue}`);
+  }
